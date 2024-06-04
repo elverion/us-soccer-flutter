5ad5e1ff679add9a24ae89c43a706e3add3709ab
Scott <scott.w.brown@gmail.com>
2024-06-03 23:32:36 -0300

    env dart small update

--- lib/const/environmentals.dart
+++ lib/const/environmentals.dart
@@ -1,4 +1,5 @@
+/// TODO: Move to .env and load
 // Standard
-// const String serverURL = 'http://127.0.0.1:8080';
+const String serverURL = 'http://127.0.0.1:8080';
 // Android Emulator
-const String serverURL = 'http://10.0.2.2:8080';
+// const String serverURL = 'http://10.0.2.2:8080';
